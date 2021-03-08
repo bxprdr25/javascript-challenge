e4bc5ad6620dbf3df44f85ecb1db8cd3c4a6825e
Manuel Mejia <bxprdr25@hotmail.com>
2021-03-08 10:23:49 -0500

    Initial commit, new css styling and html layout

---
+++ UFO-1/static/js/app.js
@@ -0,0 +1,37 @@
+// ### Level 1: Automatic Table and Date Search (Required)
+
+// * Create a basic HTML web page or use the [index.html](StarterCode/index.html) file provided (we recommend building your own custom page!).
+
+// * Using the UFO dataset provided in the form of an array of JavaScript objects, write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
+
+//   * Make sure you have a column for `date/time`, `city`, `state`, `country`, `shape`, and `comment` at the very least.
+
+// * Use a date form in your HTML document and write JavaScript code that will listen for events and search through the `date/time` column to find rows that match user input.
+
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+d3.select("tbody")
+  .selectAll("tr")
+  .data(tableData)
+  .enter()
+  .append("tr")
+  .html(function(d) {
+    return `<td>${d.datetime}</td><td>${d.city}</td><td>${d.state}</td><td>${d.country}</td><td>${d.shape}</td><td>${d.durationMinutes}</td><td>${d.comments}</td>`;
+  });
+
+var inputText = d3.select("#datetime")
+var button = d3.select("filter-btn")
+
+// filter data with desired date
+function changeHandler(){
+    d3.event.preventDefault();
+    console.log(inputText.property("value"));
+    var new_table = tableData.filter(sighting => sighting.datetime===inputText.property("value"))
+    displayData(new_table)
+}
+
+// event listener to handle change and click
+inputText.on("change", changeHandler)
+button.on("click", changeHandler)
