d4eb89924991db456c862a670968722fbfd7ef83
Manuel Mejia <bxprdr25@hotmail.com>
2021-03-09 13:03:31 -0500

    Further changes to CSS and app.js files

--- UFO-1/static/js/app.js
+++ UFO-1/static/js/app.js
@@ -12,26 +12,42 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-d3.select("tbody")
-  .selectAll("tr")
-  .data(tableData)
-  .enter()
-  .append("tr")
-  .html(function(d) {
-    return `<td>${d.datetime}</td><td>${d.city}</td><td>${d.state}</td><td>${d.country}</td><td>${d.shape}</td><td>${d.durationMinutes}</td><td>${d.comments}</td>`;
-  });
+function display(){
+  d3.select("tbody")
+    .selectAll("tr")
+    .data(tableData)
+    .enter()
+    .append("tr")
+    .html(function(d) {
+      return `<td>${d.datetime}</td><td>${d.city}</td><td>${d.state}</td><td>${d.country}</td><td>${d.shape}</td><td>${d.durationMinutes}</td><td>${d.comments}</td>`;
+    })};
 
-var inputText = d3.select("#datetime")
-var button = d3.select("filter-btn")
+    
+// function display(){
+//   var tbody = d3.select("tbody");
+//   tableData.forEach((sighting) => {
+//   var row = tbody.append("tr");
+//   Object.entries(sighting).forEach(([key, value]) => {
+//     var cell = row.append("td");
+//     cell.text(value);
+//   });
+// })};
 
-// filter data with desired date
-function changeHandler(){
-    d3.event.preventDefault();
-    console.log(inputText.property("value"));
-    var new_table = tableData.filter(sighting => sighting.datetime===inputText.property("value"))
-    displayData(new_table)
-}
+display();
 
-// event listener to handle change and click
-inputText.on("change", changeHandler)
-button.on("click", changeHandler)
+
+
+  var inputText = d3.select("#datetime")
+  var button = d3.select("filter-btn")
+
+  // filter data with desired date
+  function changeHandler(){  
+      d3.event.preventDefault();
+      console.log(inputText.property("value"));
+      var new_table = tableData.filter(sighting => sighting.datetime===inputText.property("value"))
+      display(new_table)
+  };
+  
+  // event listener to handle change and click
+  inputText.on("change", changeHandler)
+  button.on("click", changeHandler)
