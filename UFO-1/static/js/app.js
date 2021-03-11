681e96f14e55c420c5fafe69035949939d3c7e1b
Manuel Mejia <bxprdr25@hotmail.com>
2021-03-11 01:20:00 -0500

    Finished the CSS styling and additional filters in the app.js

--- UFO-1/static/js/app.js
+++ UFO-1/static/js/app.js
@@ -1,30 +1,10 @@
-// ### Level 1: Automatic Table and Date Search (Required)
-
-// * Create a basic HTML web page or use the [index.html](StarterCode/index.html) file provided (we recommend building your own custom page!).
-
-// * Using the UFO dataset provided in the form of an array of JavaScript objects, write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
-
-//   * Make sure you have a column for `date/time`, `city`, `state`, `country`, `shape`, and `comment` at the very least.
-
-// * Use a date form in your HTML document and write JavaScript code that will listen for events and search through the `date/time` column to find rows that match user input.
-
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-function display(){
-  d3.select("tbody")
-    .selectAll("tr")
-    .data(tableData)
-    .enter()
-    .append("tr")
-    .html(function(d) {
-      return `<td>${d.datetime}</td><td>${d.city}</td><td>${d.state}</td><td>${d.country}</td><td>${d.shape}</td><td>${d.durationMinutes}</td><td>${d.comments}</td>`;
-    })};
-
-    
-// function display(){
-//   var tbody = d3.select("tbody");
+// YOUR CODE HERE!   
+// function display(tableData){
+//   var tbody = d3.select("tbody")
+//   tbody.text("")
 //   tableData.forEach((sighting) => {
 //   var row = tbody.append("tr");
 //   Object.entries(sighting).forEach(([key, value]) => {
@@ -33,21 +13,46 @@ function display(){
 //   });
 // })};
 
-display();
+//Function to display all the data in data.js file and dynamically inseart/update the HTML table rows
+function display(tableData){
+  d3.select("tbody")
+    .text("")
+    .selectAll("tr")
+    .data(tableData)
+    .enter()
+    .append("tr")
+    .html(function(d) {
+      return `<td>${d.datetime}</td><td>${d.city}</td><td>${d.state}</td><td>${d.country}</td><td>${d.shape}</td><td>${d.durationMinutes}</td><td>${d.comments}</td>`;
+    })};
+
+//Call the display function
+display(tableData);
 
 
-
-  var inputText = d3.select("#datetime")
-  var button = d3.select("filter-btn")
+  var inputDate = d3.select("#datetime")
+  var inputText = d3.selectAll("input")
+  var buttonDate = d3.select("filter-btn")
+  var buttonClear = d3.select("clearFilter")
 
   // filter data with desired date
   function changeHandler(){  
       d3.event.preventDefault();
-      console.log(inputText.property("value"));
-      var new_table = tableData.filter(sighting => sighting.datetime===inputText.property("value"))
-      display(new_table)
-  };
+      console.log(inputDate.property("value"));
+      var new_table = tableData.filter(sighting => sighting.datetime===inputDate.property("value"))
+      display(new_table);
+  }
   
   // event listener to handle change and click
-  inputText.on("change", changeHandler)
-  button.on("click", changeHandler)
+  inputDate.on("change", changeHandler) 
+  inputText.on("change", additionalFilters)
+  buttonDate.on("click", changeHandler)
+  buttonClear.on("click", display)
+
+  //Function to handle additional filters for other columns in the table
+  function additionalFilters() {
+    var key = d3.select(this).property('id');
+    var value = d3.select(this).property('value');
+
+    tableData = tableData.filter(obj => obj[key] == value);
+    display(tableData);
+};
